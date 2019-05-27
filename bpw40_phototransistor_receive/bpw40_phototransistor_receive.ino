bf3b3f8291ae6b3879d2300704e4d2cc7b2bbe17
Tomash <tomekrs@o2.pl>
2019-05-27 12:24:56 +0200

    bpw40 phototransistor: connect D1 digital input alongside A0 ADC to tune for using digital input only

--- bpw40_phototransistor_receive/bpw40_phototransistor_receive.ino
+++ bpw40_phototransistor_receive/bpw40_phototransistor_receive.ino
@@ -1,20 +1,23 @@
-int ledpin = 4;
 int phototran = A0; // ADC
+int photodig  = D1; // digital input
 int interval = 100; //miliseconds
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(ledpin, OUTPUT);
+  pinMode(photodig, INPUT);
   pinMode(phototran, INPUT);
 
   Serial.begin(115200);        //inicjalizacja monitora szeregowego
-  Serial.println("Test fototranzystora");
+  Serial.println("Phototransistor test");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
   int lightval = analogRead(phototran);
+  int digval = digitalRead(photodig);
+  Serial.print(digval);
+  Serial.print(" digital, analog A0: ");
   Serial.println(lightval);
   delay(interval);
   
